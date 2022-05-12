47d64b4b3e715585aa645b344f863b50a10c2ca3
Luis Sandoval <luismsandoval96@gmail.com>
2022-05-11 21:39:50 -0700

    completed map component to render map image

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React from 'react';
 import Explore from './Explore';
+import Map from './Map';
 
 class Main extends React.Component {
 
@@ -17,6 +18,7 @@ class Main extends React.Component {
   getLocation = async (event) => {
     event.preventDefault();
     const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
+
     const response = await axios.get(url);
     this.setState({ locationName: response.data[0].display_name, lat: response.data[0].lat, lon: response.data[0].lon});
     console.log(this.state, response.data);
@@ -31,6 +33,8 @@ class Main extends React.Component {
     return (
     <>
       <Explore locationName= {this.state.locationName} lat={this.state.lat} lon={this.state.lon} searchQuery= {this.state.searchQuery} getLocation= {this.getLocation} searchLocation= {this.searchLocation} />
+
+      <Map lat={this.state.lat} lon={this.state.lon}/>
     </>
   );
   }
---
+++ src/Map.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Map extends React.Component {
+  render() {
+    return (
+    <>
+      <img alt='map' src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.props.lat},${this.props.lon}&zoom=18&size=<width>x<height>&format=<format>&maptype=<MapType>&markers=icon:<icon>`}/>
+    </>
+  );
+  }
+}
+
+export default Map;
