c85e3354cc450f6ad1c23670831803a46aa338ae
Luis Sandoval <luismsandoval96@gmail.com>
2022-05-11 20:49:34 -0700

    built out main component and explore form component with event functions

---
+++ src/Explore.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+class Explore extends React.Component {
+  render() {
+    return (
+    <>
+      <Form>
+        <Form.Group>
+          <Form.Label>search city</Form.Label>
+          <Form.Control  
+          onChange={this.props.searchLocation} 
+          placeholder='enter city' />
+        </Form.Group>
+        <Button onClick={this.props.getLocation} variant='primary' type='submit '>
+          Explore!
+        </Button>
+        {this.props.locationName && 
+          <h2>The city we searched for is {this.props.locationName} {this.props.lat} {this.props.lon}</h2>
+        }
+      </Form>
+    </>
+  );
+  }
+}
+
+export default Explore;
