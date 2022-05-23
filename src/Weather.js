8d20516acb86dc206dec2e4b8223fe2da499864f
Luis Sandoval <luismsandoval96@gmail.com>
2022-05-23 14:17:11 -0700

    fixed weather day rendering issues

--- src/Weather.js
+++ src/Weather.js
@@ -4,11 +4,12 @@ import WeatherDay from './WeatherDay';
 
 class Weather extends React.Component {
   render() {
+    console.log(this.props.weather);
     return (
     <>
       {this.props.weather &&
       this.props.weather.map(value => (
-        <WeatherDay date={value.date} description={value.description} />
+        <WeatherDay date={value.datetime} description={value.description} />
       ))
       }
     </>
